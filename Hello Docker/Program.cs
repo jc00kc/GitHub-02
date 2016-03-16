bc205cfea350adc4ec5de23f6f7215445d8be988
jc00kc <jc00kc@hotmail.com>
2016-03-16 15:10:07 -0400

    Version 0.1

---
+++ Hello Docker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Docker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello Docker! v0.1");
+		}
+	}
+}
